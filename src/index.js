a2a6dc74e9759d4ae11f2138fe513cac0c0634eb
andrewswint <andrewswint1976@gmail.com>
2019-11-14 15:56:13 -0600

    export stuff

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const $ = require('jquery');
+import sayHello from "./say-hello.js";
 $(document).ready(function () {
-    let sayHello = () => {return console.log('hello')};
-
-    sayHello();
+    // let sayHello = () => {return console.log('hello')};
+    //
+    // sayHello();
 
     $('body').css({
         "background-color": "blue"
     });
 });
+console.log(sayHello());
 
 
 
---
+++ src/say-hello.js
@@ -0,0 +1,5 @@
+const sayHello = () => {
+    return "Nice message"
+};
+
+export default sayHello;
